f1f0d0148670a1b01c430a5b9018ebe52ba17fa5
Your Name <meghdiproy23234@gmail.com>
2024-06-20 13:01:09 +0530

    index.js file changed

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -18,7 +18,8 @@ root.render(
  // </React.StrictMode>
 );
 
+
+reportWebVitals();
 // If you want to sACtart measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
